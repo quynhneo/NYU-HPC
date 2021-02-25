5335137b2ff556774176d3525c505622e03f3650
Quynh Minh Nguyen <qmn203@log-2.nyu.cluster>
2021-02-24 19:40:06 -0500

    add better version of squash

---
+++ squashing_2.sh
@@ -0,0 +1,18 @@
+# script to squash every sub-sub directory (depth=3), with parent='pdf', name 4 characters 
+# long 'YYMM' in pwd into sqf files
+# mksquashfs automatically use all available processors
+for d in $(find . -maxdepth 3 -mindepth 3 -name '????' ) ; do  #select only folder
+	echo "${d%/}" #remove trailing slash
+	if [ -f "${d%/}.sqf" ]; then
+		echo "the folder has been squashed. Skipping"
+	else
+		echo "the folder has not been squashed. Squashing..."
+		#if mksquashfs $d "${d%/}.sqf" -keep-as-directory ; then 
+			#echo "squashing succeeded, deleting source"
+			#rm -r $d #remove this line if unwanted!!!!
+		#else
+			#echo "squashing failed"
+		#fi
+	fi
+		
+done
