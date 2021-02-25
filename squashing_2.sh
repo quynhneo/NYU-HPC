57a0f5eaf10bb2f914d50ec4d0663f9dbd634b57
quynhneo <qmn203@log-2.nyu.cluster>
2021-02-24 20:32:43 -0500

    comment

--- squashing_2.sh
+++ squashing_2.sh
@@ -1,5 +1,5 @@
-# script to squash every sub-sub directory (depth=3) of pwd, with parent='pdf', name 4 characters 
-# long (matching the 'YYMM' format) into sqf files
+# script to squash every sub-sub directory (depth=3) of pwd, name 4 characters 
+# long (matching the 'YYMM' format), into sqf files
 # mksquashfs automatically use all available processors
 for d in $(find . -maxdepth 3 -mindepth 3 -name '????' ) ; do  #select only folder
 	echo "${d%/}" #remove trailing slash
