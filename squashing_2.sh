ef9814c3745f03e5ba2ae03da8e3b61e56cff382
quynhneo <qmn203@nyu.edu>
2021-02-24 20:49:47 -0500

    find -path option instead of -name

--- squashing_2.sh
+++ squashing_2.sh
@@ -1,8 +1,11 @@
 # 
-# script to squash every sub-sub directory (depth=3) of pwd, name 4 characters 
-# long (matching the 'YYMM' format), into sqf files
+# script to squash every 
+# sub-sub directory (depth=3) of pwd, 
+# name 4 characters 
+# long (matching the '*/pdf/????' format), 
+# into sqf files
 # mksquashfs automatically use all available processors
-for d in $(find . -maxdepth 3 -mindepth 3 -name '????' ) ; do  #select only folder
+for d in $(find . -maxdepth 3 -mindepth 3 -path '*pdf/????' ) ; do  #select only folder
 	echo "${d%/}" #remove trailing slash
 	if [ -f "${d%/}.sqf" ]; then
 		echo "the folder has been squashed. Skipping"
